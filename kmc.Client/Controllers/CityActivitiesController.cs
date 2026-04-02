c123bee800a78bac5675a842389acdb4327c5a12
Senira Mendis <seniramendis41@gmail.com>
2026-04-03 00:33:04 +0530

    commit upto now

---
+++ kmc.Client/Controllers/CityActivitiesController.cs
@@ -0,0 +1,38 @@
+﻿using kmc.Client.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace kmc.Client.Controllers
+{
+    public class CityActivitiesController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly IConfiguration _configuration;
+
+        public CityActivitiesController(IHttpClientFactory httpClientFactory, IConfiguration configuration)
+        {
+            _httpClientFactory = httpClientFactory;
+            _configuration = configuration;
+        }
+
+        // GET: CityActivities
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var baseUrl = _configuration["ApiSettings:BaseUrl"];
+
+            // This calls your API to get the data
+            var response = await client.GetAsync($"{baseUrl}/api/CityActivities");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonString = await response.Content.ReadAsStringAsync();
+                var activities = JsonSerializer.Deserialize<List<CityActivityViewModel>>(jsonString, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+                return View(activities);
+            }
+
+            // If the API call fails, return an empty list so the page doesn't crash
+            return View(new List<CityActivityViewModel>());
+        }
+    }
+}
--- kmc.Client/Program.cs
+++ kmc.Client/Program.cs
@@ -9,13 +9,15 @@ namespace kmc.Client
             // Add services to the container.
             builder.Services.AddControllersWithViews();
 
+            // REQUIRED: Allow the client to make HTTP calls to the API
+            builder.Services.AddHttpClient();
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
